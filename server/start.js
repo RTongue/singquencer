46854957e6102584a2a407f60e25def7b7e5f924
RTongue <r.tongue@gmail.com>
2017-06-06 14:26:36 -0400

    Cut dead code from server

--- server/start.js
+++ server/start.js
@@ -13,16 +13,12 @@ if (process.env.NODE_ENV !== 'production') {
   // Logging middleware (non-production only)
   app.use(morgan('dev'));
 }
-//The code below works because `.use` returns `this` which is `app`. So what we want to return in the `module.exports` is `app`, and we can chain on that declaration because each method invokation returns `app` after mutating based on the middleware functions
+// The code below works because `.use` returns `this` which is `app`. So what we want to return in the `module.exports` is `app`, and we can chain on that declaration because each method invokation returns `app` after mutating based on the middleware functions
 module.exports = app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use(express.static(resolve(__dirname, '..'))) // Serve static files from ../dist
   .use(express.static(resolve(__dirname, '..', 'public'))); // Serve static files from ../public
-  // .use('/api', require('./api')) // Serve our api (none yet...)
-  // .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html'))); // Send index.html for any other requests.
-
-  // notice the use of `_` as the first parameter above. This is a pattern for parameters that must exist, but you don't use or reference (or need) in the function body that follows.
 
 if (module === require.main) {
   // Start listening only if we're the main module.
